b07c955b93efc00d95b686347bea8fb16334c498
Ornella <pilegiorne@gmail.com>
2022-07-18 22:05:44 -0300

    Archivos

---
+++ app.js
@@ -0,0 +1,80 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+require('dotenv').config();
+
+var session = require('express-session');
+
+var indexRouter = require('./routes/index');
+var nosotrosRouter = require('./routes/nosotros');
+var serviciosRouter = require('./routes/servicios');
+var adopcionesRouter = require('./routes/adopciones');
+var galeriaRouter = require('./routes/galeria');
+var turnosRouter = require('./routes/turnos');
+var loginRouter = require('./routes/admin/login');
+var adminAdopcionesRouter = require('./routes/admin/adopciones');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'gfd6gf6gfsd5f5g6sfdfd7sgf6d',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}));
+
+secured = async function (req, res, next) {
+  try {
+    console.log(req.session.id_usuario)
+
+    if (req.session.id_usuario) {
+      next()
+    } else {
+      res.redirect('/admin/login')
+    }
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+app.use('/', indexRouter);
+app.use('/nosotros', nosotrosRouter);
+app.use('/servicios', serviciosRouter);
+app.use('/adopciones', adopcionesRouter);
+app.use('/galeria', galeriaRouter);
+app.use('/turnos', turnosRouter);
+app.use('/admin/login', loginRouter);
+app.use('/admin/adopciones', secured, adminAdopcionesRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
